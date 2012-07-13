ac773f31038d8143b3606fbc9830cb5ee2bf9b90
Daryl Teo <i.am@darylteo.com>
2012-07-13 23:35:37 +1000

    Version 2 - unstable

--- nyaa.py
+++ nyaa.py
@@ -1,5 +1,6 @@
 # encoding: utf-8
 
+# imports
 import urllib2
 import feedparser
 import simplejson as json
@@ -8,161 +9,63 @@ import os
 
 import sched,time
 
-def initSettings(url):
-   if(not os.path.isfile(url)):
-      f = open(url,'w')
-      f.write(
-         json.dumps({
-            "subscriptions_file": "subscriptions.json",
-            "target_url": "target",
-            "history_file": "history.json"
-         })
-      )
-      f.close()
-
-def initFiles(subs_url,history_url,target_url):
-   if(not os.path.isfile(subs_url)):
-      f = open(subs_url,'w')
-      f.write('[]')
-      f.close()
-
-   if(not os.path.isfile(history_url)):
-      f = open(history_url,'w')
-      f.write('[]')
-      f.close()
-
-   if(not os.path.isdir(target_url)):
-      os.makedirs(target_url)
-
-
-def isSubbed(subs,title):
-   for sub in subs:
-      matches = re.match(sub,title)
-      if matches:
-         return (True,int(matches.group('episode')))
-
-   return (False,'-1')
-
-def loadSubscriptions(url):
-   f = open(url,'r')
-   subs = json.loads(
-      f.read() 
-   )
-   f.close()
-
-   return subs
-
-def loadSettings(url):
-   f = open(url,'r')
-   settings = json.loads(
-      f.read()
-   )
-   f.close()
-
-   return settings
-
-def loadFeed(url):
-   return feedparser.parse(url)
-
-
-def loadHistory(url):
-   f = open(url,'r')
-   history = json.loads(
-      f.read()
-   )
-   f.close()
-
-   return set(history)
-
-def saveHistory(history,url):
-   f = open(url,'w')
-   f.write(
-      json.dumps(list(history))
-   )
-   f.close()
-  
-
-def downloadToTarget(url,filename,target_url):
-   f = urllib2.urlopen(url)
-   data = f.read()
-   f.close()
-
-   filename = os.path.join(target_url,filename)
-   f = open(filename,'w')
-   f.write(data)
-   f.close()
-   
-
-def doUpdateSub(subscription, target_dir, history):
-
-   def buildRegex(expression):
-      expression = re.sub(r"[\[\]\.]",r'\\\g<0>', expression)
-      expression = re.sub(r'{{(.+?)}}',r'(?P<\g<1>>.+?)',expression)
-      return expression
-
-   data = loadFeed(subscription['feed'])
-   subs = subscription['subs']
-   feed_url = subscription['feed']
-
-   # compile subscription templates into regex expressions
-   subs = map(buildRegex,subs)
-
-   for entry in data.entries:
-      subscribed,episode = isSubbed(subs,entry.title)
-
-      if(not subscribed):
-         continue
-
-      if(entry.title in history):
-         continue
-
-      # download the torrent
-      downloadToTarget(
-         entry.link + '.torrent',
-         entry.title,
-         target_dir 
-      )
-
-      # if successful download then add to history
-      history.add(entry.title)
-
-      print 'Downloaded ' + entry.title
-
-
-def doUpdate(settings,scheduler):
-   scheduler.enter(
-      60,  # 1 minutes
-      1,   # priority 1
-      doUpdate,
-      (settings,scheduler)   # params
-   )
-
-   subs_url = settings['subscriptions_file']
-   history_url = settings['history_file']
-   target_dir = settings['target_dir']
-
-   initFiles(
-      subs_url,
-      history_url,
-      target_dir
-   )
-
-   history = loadHistory(history_url)
-   subs = loadSubscriptions(subs_url)
-
-   for sub in subs:
-      doUpdateSub(sub,target_dir,history)
-
-   saveHistory(history,history_url)
-
 def main():
-   initSettings('settings.json')
-   settings = loadSettings('settings.json')
+  
+   def getSettings():
+      print("Get Settings")
 
-   scheduler = sched.scheduler(time.time,time.sleep)
-   scheduler.enter(60,1,doUpdate,(settings,scheduler))
-   scheduler.run()
+   def getSubscriptions(filename):
+      with open(filename,"r") as f:
+         return json.loads(
+            f.read()
+         )
 
+   def getFeed(url):
+      print("Read Feed: " + url)
+
+      return feedparser.parse(url)
+
+   def checkSubscriptions(subscriptions):
+      def buildRegex(expression):
+         expression = re.sub(r"[\[\]\.]",r'\\\g<0>', expression)
+         expression = re.sub(r'{{(.+?)}}',r'(?P<\g<1>>.+?)',expression)
+         return expression
+
+      def isSubbed(subs,entry):
+         for sub in subs:
+            if re.match(sub,entry.title):
+               return entry
+
+      for group in subscriptions:
+         print("Checking Subs for '" + group["name"] + "'")
+
+         titles = map(
+            buildRegex,
+            group["subs"]
+         )
+         print(titles) 
+         feed = getFeed(group["url"]) 
+
+         matches = [
+            entry for entry in 
+            [
+               isSubbed(titles,entry) for entry in feed.entries 
+            ]
+            if entry != None
+         ]
+
+         downloadFiles(matches)
+
+   def downloadFiles(entries):
+      print("Download Files")
+
+      for entry in entries:
+         print entry.title,entry.link
+
+   checkSubscriptions(
+      getSubscriptions(
+         "subscriptions.json"
+      )
+   )
 
 main()
-   
